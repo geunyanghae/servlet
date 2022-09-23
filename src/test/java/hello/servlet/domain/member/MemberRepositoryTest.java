8edea983a64cc50e3fb0a89204327afcc7ba872c
dobbiDaddy <ahnsang2107@gmail.com>
2022-09-23 16:59:42 +0900

    servlet

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,6 +1,6 @@
 package hello.servlet.domain.member;
 
-import org.assertj.core.api.Assertions;
+  import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
